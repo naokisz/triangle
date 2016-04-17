8521f7a65a89d5e01e4e683521100c3c38f47810
naokisz <naokihirokitomoki@gmail.com>
2016-04-17 22:33:12 +0900

    取り敢えず書き終わった。デバッグ中。

--- triangle.c
+++ triangle.c
@@ -2,94 +2,253 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-	int number, layer = 0, start = -1, goal = -1, step = -1;
-	int pow_tmp, rand_tmp;
+	if(argc == 2 && strcmp(argv[1], "--help") == 0) {
+
+		printf("ヘルプ\n\n");
+		printf("このプログラムについて\n");
+		printf("（確か）東大のある入試問題をパソコンで解こうとしてみただけです。\n\n");
+		printf("オプション\n");
+		printf("--help：このヘルプを表示します。\n\n");
+
+		return 0;
+	}
+
+	int layer = -1, number = -1, goal = -1, step = -1;
+	int try_times = -1, pow_tmp, rand_tmp, reached_times = 0;
+	int i, j, k;
+	double probability = 0;
 
 	srand(time(NULL));
 
 	for(;;) {
-		printf("何層の三角形を積んだ三角形にしますか？＞");
+		printf("何層の三角形を積んだ三角形にしますか？");
+		printf("ランダムに決めても良い場合は０を入力してください。＞");
 		scanf("%d", &layer);
 		while(getchar() != '\n') { }
-		if(layer > 0) break;
+
+		if(layer == 0) {
+			/*46340:int型で2乗してもオーバーフローしない限界*/
+			layer = (int)(((double)rand() / RAND_MAX) * 46340) + 1;
+			printf("%d層\n", layer);
+		}
+
+		if(layer > -1) break;
+
+		printf("もう一度入力してください。\n");
 	}
 
 	for(;;) {
-		printf("スタートする番号を入力してください。ランダムに決めても良い場合は０を入力してください。＞");
-		scanf("%d", &start);
+		printf("スタートする番号を入力してください。");
+		printf("ランダムに決めても良い場合は０を入力してください。＞");
+		scanf("%d", &number);
 		while(getchar() != '\n') { }
 
-		if(start == 0) {
-			start = rand() % (pow(layer, 2) + 1);
+		if(number == 0) {
+			number = (int)(((double)rand() / RAND_MAX) * pow(layer, 2)) + 1;
+			printf("%d\n", number);
 		}
-		number = start;
 
-		if(start > -1) break;
+		if(number > -1) break;
+
+		printf("もう一度入力してください。\n");
 	}
 
 	for(;;) {
-		printf("ゴールの番号を入力してください。ランダムに決めても良い場合は０を入力してください。＞");
+		printf("ゴールの番号を入力してください。");
+		printf("ランダムに決めても良い場合は０を入力してください。＞");
 		scanf("%d", &goal);
 		while(getchar() != '\n') { }
 
 		if(goal == 0) {
-			goal = rand() % (pow(layer, 2) + 1);
+			goal = (int)(((double)rand() / RAND_MAX) * pow(layer, 2)) + 1;
+			printf("%d\n", goal);
 		}
 
 		if(goal > -1) break;
+
+		printf("もう一度入力してください。\n");
 	}
 
 	for(;;) {
-		printf("何ステップ動かしますか？ランダムに決めても良い場合は０を入力してください。＞");
+		printf("一回で何ステップ動かしますか？");
+		printf("ランダムに決めても良い場合は０を入力してください。＞");
 		scanf("%d", &step);
 		while(getchar() != '\n') { }
-		
+
 		if(step == 0) {
 			step = rand();
+			printf("%dステップ\n", step);
 		}
 
 		if(step > -1) break;
+
+		printf("もう一度入力してください。\n");
 	}
 
-	for(i = 0; i < step; i++) {
+	for(;;) {
+		printf("何回実行しますか？");
+		printf("ランダムに決めても良い場合は０を入力してください。＞");
+		scanf("%d", &try_times);
 
-		/*三角形の頂点にいたら、決まった場所に移動する。*/
-		if(number == 1) number = 3;
-
-		else if(number == pow(layer, 2)) {
-			number = pow(layer, 2) - 1;
+		if(try_times == 0) {
+			try_times = rand();
+			printf("%d回\n", try_times);
 		}
 
-		else if(number == (pow(layer - 1, 2) + 1)) {
-			number = pow(layer - 1, 2) + 1;
-		}
+		if(try_times > -1) break;
 
-		/*三角形の端にいたら、動ける場所を制限される。*/
-		for(j = 1; j <= layer; j++) {
+		printf("もう一度入力してください。\n");
+	}
 
-			pow_tmp = pow(i, 2);
+	for(i = 0; i < try_times; i++) {
+		for(j = 0; j < step; j++) {
 
-			if(number == pow_tmp) {
+			/*三角形の頂点にいたら、決まった場所に移動する。*/
+			if(number == 1) {
+				number = 3;
+				goto continue_step;
+			}
+
+			/*1の時の処理をforで回さないと
+				2の時の処理が出来ないのでここに書く*/
+			else if(number == 2) {
 
 				rand_tmp = rand() % 2;
 
 				if(rand_tmp == 0) {
-					number = number - 1;
-					break;
+					number++;
+					goto continue_step;
 				}
 
-				if else(rand_tmp == 1) {
-					number = number + pow_tmp + 1;
-					break;
+				else if(rand_tmp == 1) {
+					number = 6;
+					goto continue_step;
 				}
 			}
 
-			else if(number == pow_tmp + 1) {
-				
-				rand_tmp = rand() % 2;
+			else if(number == pow(layer, 2)) {
+				number = pow(layer, 2) - 1;
+				goto continue_step;
+			}
 
-				if(rand_tmp == 0) {
-					number = number + 
+			else if(number == (pow(layer - 1, 2) + 1)) {
+				number = pow(layer - 1, 2) + 1;
+				goto continue_step;
+			}
+
+			/*三角形の端にいたら、動ける場所を制限される。*/
+			for(k = 2; k <= layer; k++) {
+
+				pow_tmp = pow(k, 2);
+
+				if(number == pow_tmp) {
+
+					rand_tmp = rand() % 2;
+
+					if(rand_tmp == 0) {
+						number--;
+						goto continue_step;
+					}
+
+					else if(rand_tmp == 1) {
+						number += pow_tmp - pow(k - 1, 2) + 1;
+						goto continue_step;
+					}
+				}
+
+				else if(number == pow_tmp + 1) {
+
+					rand_tmp = rand() % 2;
+
+					if(rand_tmp == 0) {
+						number++;
+						goto continue_step;
+					}
+
+					else if(rand_tmp == 1) {
+						number += (pow(k + 1, 2) - pow(k, 2)) + 1;
+						goto continue_step;
+					}
+				}
+			}
+
+			/*三角形の下端*/
+			for(k = pow(layer - 1, 2) + 1; k < pow(layer, 2); k += 2) {
+
+				if(number == k) {
+
+					rand_tmp = rand() % 2;
+
+					if(rand_tmp == 0) {
+						number--;
+						goto continue_step;
+					}
+
+					else if(rand_tmp == 1) {
+						number++;
+						goto continue_step;
+					}
+				}
+			}
+
+			/*三方向どこにも動ける三角形*/
+			rand_tmp = rand() % 3;
+
+			if(rand_tmp == 0) {
+				number--;
+				goto continue_step;
+			}
+
+			else if(rand_tmp == 1) {
+				number++;
+				goto continue_step;
+			}
+
+			/*XXX:予想以上にゴリ押しなコードになったので要修正?*/
+			else if(rand_tmp == 2) {
+
+				/*numberの一個前の右端の数を求める。*/
+				for(k = 1; k < number; k++) {
+					pow_tmp = pow(k, 2);
+					if(pow_tmp > number) {
+						pow_tmp = pow(k - 1, 2);
+						break;
+					}
+				}
+
+				/*三角形が上向き、下向きかで動きを決める*/
+				if(((number - (pow_tmp + 1)) % 2) == 0) {
+					number += (pow(k, 2) - pow(k - 1, 2)) + 1;
+					goto continue_step;
+				}
+				else if(((number - (pow_tmp + 1)) % 2) == 1) {
+					number -= (pow(k - 1, 2) - pow(k - 2, 2)) + 1;
+					goto continue_step;
+				}
+			}
+
+			continue_step:
+			continue;
+		}
+
+		if(number == goal) {
+			reached_times++;
+			printf("ゴールに到達しました。\n");
+		}
+
+		if(probability != 0) {
+			probability = (double)reached_times / try_times;
+		}
+		printf("試行回数：%d回 ゴールに到達した回数：%d回 確率：%lf\n"
+			,try_times, reached_times, probability);
+	}
+
+	printf("試行が終了しました。\n");
+
+	return 0;
+}
+
